9becb0ddf464f99cc00d9fbe693498d01514936a
pathalaprasad <pathalaprasad2016jag@gmail.com>
2026-01-20 15:16:09 +0530

    Added InsertionSort.java

--- src/dsa/sorting/InsertionSort.java
+++ src/dsa/sorting/InsertionSort.java
@@ -10,7 +10,7 @@ public class InsertionSort {
             int key = arr[i];
             int j = i - 1;
 
-            /* Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position */
+            /* Move elements of arr[0...i-1], that are greater than key, to one position ahead of their current position */
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
@@ -28,7 +28,7 @@ public class InsertionSort {
 
     // Driver method
     public static void main(String[] args) {
-        int arr[] = {12, 11, 13, 5, 6};
+        int[] arr = {12, 11, 13, 5, 6};
 
         System.out.println("Original array:");
         printArray(arr);
